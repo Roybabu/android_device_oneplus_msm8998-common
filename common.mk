30a35cdb19e5fd34469bea6f4fb815bf4fa98eee
Dabug123 <clarkbrunswick55@gmail.com>
2018-03-21 05:55:30 +0100

    prebuilt:add oneplusgallery

--- common.mk
+++ common.mk
@@ -156,8 +156,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    OnePlusGallery \
-    GoogleCameraMod
+    GoogleCameraMod \
+    OnePlusGallery
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0 \
