61ac8317ef2cfb3f07cf3c27b906ecd4c35349b0
Roybabu <fb.riki.fb@gmail.com>
2018-08-22 20:47:33 +0000

    Revert "msm8998: Build LiveDisplay HAL"
    This reverts commit a37c09447426d591d1a6d779f5d8a7fa1270c6c9.

--- common.mk
+++ common.mk
@@ -306,11 +306,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay native
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm \
-    vendor.lineage.livedisplay-V1.0-java
-
-PRODUCT_BOOT_JARS += \
-    vendor.lineage.livedisplay-V1.0-java
+    libjni_livedisplay
 
 # Live Wallpapers
 PRODUCT_PACKAGES += \
